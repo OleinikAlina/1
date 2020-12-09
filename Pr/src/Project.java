8f7ce125c34a19074c5d4f744ef92e0b97079706
OleinikAlina <a.oleinik@g.nsu.ru>
2020-12-09 19:21:40 +0700

    ООАиД

--- Pr/src/Project.java
+++
@@ -1,9 +0,0 @@
-import Window.Menu;
-
-public class Project {
-    public static void main(String[] args){
-        new Menu();
-    }
-}
-//Rating.Rating  - fileOfDoc - имя файла и путь, где хранится список последних просмотренных айлов
-// DownloadMouseL - обработка мени файла
